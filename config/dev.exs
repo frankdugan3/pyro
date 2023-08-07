193c5f3ebd5cc5d948f71f25609f4ba9b731230e
Frank Dugan III <frank3@duganator.com>
2023-08-07 09:31:27 -0500

    chore: upgrade deps

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,7 @@ if Mix.env() == :dev do
 
   # Configure esbuild (the version is required)
   config :esbuild,
-    version: "0.17.19",
+    version: "0.18.19",
     default: [
       args:
         ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -40,7 +40,7 @@ if Mix.env() == :dev do
 
   # Configure tailwind (the version is required)
   config :tailwind,
-    version: "3.3.2",
+    version: "3.3.3",
     default: [
       args: ~w(
         --config=tailwind.config.js
