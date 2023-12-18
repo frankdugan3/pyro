7d971b76222fcdb90fb1124510580124df495c57
Frank Dugan III <frank3@duganator.com>
2023-12-17 20:26:13 -0600

    feat: add smart_class for DSL

---
+++ lib/pyro/component/smart_component.ex
@@ -0,0 +1,18 @@
+if Code.ensure_loaded?(AshPhoenix) do
+  defmodule Pyro.Components.SmartComponent do
+    @moduledoc """
+    Utilities for building smart components.
+    """
+
+    def smart_class(fun, assigns) when is_function(fun, 1), do: fun.(assigns)
+
+    def smart_class(class, _assigns), do: class
+
+    defmacro __using__(_env) do
+      quote do
+        use Pyro.Component
+        import unquote(__MODULE__)
+      end
+    end
+  end
+end
