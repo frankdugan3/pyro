eb32af021408fa1f1258f446e74d8c9ee81259a3
Frank Dugan III <frank3@duganator.com>
2023-12-17 20:28:24 -0600

    tidbit: rename alias

--- .iex.exs
+++ .iex.exs
@@ -3,7 +3,7 @@ if function_exported?(Mix, :__info__, 1) and Mix.env() == :dev do
   Logger.configure_backend(:console, device: Process.group_leader())
 end
 
-alias Pyro.Ash.Extensions.Resource.Info, as: UI
+alias Pyro.Ash.Extensions.Resource.Info, as: PI
 alias Ash.Resource.Info, as: ResourceInfo
 alias Ash.Policy.Info, as: PolicyInfo
 alias Ash.Api.Info, as: ApiInfo
