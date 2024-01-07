431394ef81ea4eb85ce16204fb16c9056bcbb370
Frank Dugan III <frank3@duganator.com>
2024-01-07 00:05:26 -0600

    tidbit: bump elixir version

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Pyro.MixProject do
       app: :pyro,
       version: @version,
       description: @description,
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       package: package(),
       deps: deps(),
