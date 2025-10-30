655cfe872761108134d986863d5fb5d72e3b4d15
Frank Polasek Dugan III <frank3@duganator.com>
2025-10-30 15:47:04 -0500

    chore: upgrade deps

--- example/config/config.exs
+++ example/config/config.exs
@@ -33,7 +33,7 @@ config :example, Example.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.25.10",
+  version: "0.25.11",
   example: [
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.14",
+  version: "4.1.16",
   example: [
     args: ~w(
       --input=assets/css/app.css
